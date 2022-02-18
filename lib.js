3fdc72cbd3c82f4dc3723424f2e069561c377ceb
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-02-18 09:25:27 -0800

    add CHM1 callset

--- lib.js
+++ lib.js
@@ -331,6 +331,7 @@ function read_in_bed9_defaults() {
             `datasets/${REF}/${REF}_dupmasker_colors.bed`,
             `datasets/${REF}/${REF}_genes_small.bed`,
             `datasets/${REF}/${REF}_gaps.bed`,
+            `datasets/${REF}/CHM1_raw.bed9`,
         ],
         query: [
             `datasets/${QUERY}/${QUERY}_dupmasker_colors.bed`,
